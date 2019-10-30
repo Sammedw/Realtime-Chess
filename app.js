9e188b003f4e00939929de38fc9dac878fd5edd0
Sam <sam_medwell@hotmail.co.uk>
2019-10-30 23:02:07 +0000

    Basic chat

--- app.js
+++ app.js
@@ -16,5 +16,15 @@ const io = socket(server);
 
 io.on("connection", function (socket) {
     console.log("made socket connection", socket.id)
+
+    socket.on("chat", function (data) {
+        //refers to all connected sockets
+        io.sockets.emit("chat", data);
+    });
+
+    socket.on("typing", function (data) {
+        //refers to all connected sockets
+        socket.broadcast.emit("typing", data);
+    });
 });
 
