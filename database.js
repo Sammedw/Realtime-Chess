9bdb4dbb4d8692d716f8db8636410075debe40d7
Samuel Medwell <Samuel.Medwell@worthing.ac.uk>
2019-12-05 10:16:29 +0000

    Added sqlite functionality

--- database.js
+++ database.js
@@ -1,6 +1,21 @@
 const sqlite3 = require('sqlite3').verbose();
-const db = new sqlite3.Database('/chess.db', (err) => {
+const db = new sqlite3.Database('chess.db', (err) => {
     if (err) {
-    
-}
+        console.error(err.message);
+    } else {
+        console.log('Connected to databse');
+    }
+
+    const query = 'SELECT * FROM users WHERE username=?';
+
+    db.each(query, ['Bill'], (err, row) => {
+        if (err) {
+            throw err;
+        } else {
+            console.log(row.UserID, row.Username, row.Password);
+        }
+    });
+
+    db.close();
+   
 });
