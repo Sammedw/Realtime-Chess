cf34e36456ad8ff72daf8ed63cbc5e753d49cb6b
Sam <sam_medwell@hotmail.co.uk>
2020-01-23 22:17:39 +0000

    Initial Setup

---
+++ server.js
@@ -0,0 +1,15 @@
+//RTChess by Sam Medwell
+
+//Import required packages
+const express = require("express");
+
+
+//Create instance of express class
+const app = express();
+
+//Start server
+app.listen(4000, function () {
+    console.log("listening on port 4000");
+})
+
+
